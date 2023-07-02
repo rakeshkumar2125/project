54166a653e1641b506e57e53fb77798b574e08c1
rakeshkumar2125 <138386553+rakeshkumar2125@users.noreply.github.com>
2023-07-02 12:16:56 -0400

    first commit

---
+++ javascript/Quickly revise all the topics.lua
@@ -0,0 +1,209 @@
+Quickly revise all the topics 
+	HTML 
+	CSS 
+	Bootstarcp 
+	basic javaScript 
+
+
+javaScript 
+
+Bootsrcp 5: glyphicons 
+
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Thaneshwara848/SL_MeanStack.git
+git push -u origin main
+
+git init
+git add README.md
+git commit -m "first commit"
+git push 
+
+
+
+  git config --global user.email "thaneshwaramanjunatha@gmail.com"
+  git config --global user.name "Thaneshwara"
+
+
+===============================
+HTML
+                   Break (<br></br>)
+                   Paragraph (<p></p> Paragraph tag gives one more line)
+                   HR (<hr> horizontal tag gives one line)
+
+                   Closing Tags - H1-H6, Span, div, P 
+                   Non Closing Tags - HR, BR, IMG
+                   Ordered & Un-Ordered List 
+                    Table - Table. th, td , cellspacing, rowspan, colspan
+                    images :
+                    Links: 
+                    input forms 
+                    audio / vedio
+                    map : googlr map 
+                    text formating 
+                    colors
+                    local sotrgae / Session sotrgae
+                  
+HTML :
+      Hyper text markup lang
+      just only the page : no style 
+
+CSS :
+    style 
+    Cascading style sheet
+    1) it describe how html elememts are to be displayed on screen , 
+      Look and feel 
+    2) css saves a lot of work it can controle the multple page 
+    
+
+	Types of Css:
+			Inline 
+			internal / Embaded 
+			External style sheet
+
+    	Selectors : 
+    	  id 
+    	  class 
+     	 tag 
+      	multyple  tag 
+      Universal 
+
+	Background propertys 
+		bg-Image 
+		bg-repet
+		bg-attachment 
+	margin 
+		top 
+		left 
+		right 
+		bottom 
+	Padding 
+		padding top 
+			left
+			right 
+			bottom 
+	float 
+	opacity 
+	text 
+	font 
+	Box model 
+	overflow
+	
+	Animation 
+	
+
+BootStracp 
+
+CSS 
+	line by line we have to write 
+===================
+
+BootStracp 
+	we have so many ready made class 
+	Gird 
+	button
+	text 
+	bg 
+	table 
+	pagination 
+	progress bar 
+	glyphicon
+	Forms 
+	collapas
+	corosual 
+	nav bar 
+	tooltip 
+	model 
+
+				Java Script
+
+			java 								javaScript 
+
+programing 										Scripting lang 
+
+compile
+	+
+		 run 									RUN 
+
+Slow 											fast
+
+Server side 									Cleint + server : so more popualr 
+
+
+
+
+javaScript :e 
+		AngualrJS 
+		React JS 
+		nodeJS
+		ExprerssJS
+		Veniall JS 
+		VewJS
+		BackboonJS 
+		Etc...!
+
+JAVA SCRIPTS 
+
+	functions 
+		funtions wityh parameters 
+	events 
+		onlick ,ondbl ,onmoseover , onmouseout, onload , onchange, on select 
+		onsubmit, onfocus , onblue, 
+
+	logics 
+	validation
+		onsubmits 
+		name==null, undefined , o 
+		<3
+		>10 
+		patterns  
+
+	Data Types 
+		string 
+		number 
+
+	Operatoos : 
+			aretmentic
+			logical 
+			assignment operators 
+			condatinal operators 
+			etc...? 
+	statemts :if , else , swithc 
+		loop : for , whle , do while 
+		jumping : break , continu , 
+	
+	protoType 
+	CallBack funtions 
+ 
+
+DOM : Document object model 
+	id : getElementsByID();
+	name:getElementByName
+	class: getEleenmtByClassName
+	tag: getEleementByTagName()
+	InnerHtml
+	innterText
+
+
+BoM: browser Object Model :
+	window functions :
+		alert
+		confim 
+		prompt
+
+
+Advanced javaScript :
+	class 
+	object 
+	oops 
+
+
+
+
+
+
+
